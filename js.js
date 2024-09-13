bf1a5801938166bffeae03d3f19c930a44fa4d80
web-devteam <154674696+web-devteam@users.noreply.github.com>
2024-09-14 00:31:26 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,7 @@
 document.getElementById("add").addEventListener("click", ()=>{
     document.getElementById("home").style.display="none"
 })
+
+window.addEventListener("load", ()=>{
+  document.getElementById("loadpage").style.display="none"
+})
