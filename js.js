8207550c569cd07e00bb13f6108aa4f71f4b6b96
web-devteam <154674696+web-devteam@users.noreply.github.com>
2024-09-14 04:55:54 +0300

    Update js.js

--- js.js
+++ js.js
@@ -5,3 +5,19 @@ document.getElementById("add").addEventListener("click", ()=>{
 window.addEventListener("load", ()=>{
   document.getElementById("loadpage").style.display="none"
 })
+interact('.draggable').draggable({
+    listeners: {
+        start(event) {
+            console.log(event.type, event.target);
+        },
+        move(event) {
+            let target = event.target;
+            let x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+            let y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+            target.style.transform = `translate(${x}px, ${y}px)`;
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+        }
+    }
+});
